2f823aa676fe1419e8af5d156b35e68025103f87
paizanadrian <paizanadrian@gmail.com>
2024-07-03 23:17:47 +0300

    Change js code.

--- app.py
+++ app.py
@@ -78,38 +78,45 @@ def generate_html(data):
 
     html_content += """
         <script>
-                for (let i = 1; i <= 100; i++) {
-                let copyButton = document.getElementById('copy-button-' + i);
-                let myInput = document.getElementById('myInput-' + i);
-                let textDisplay = document.getElementById('text-display-' + i);
-        
-                copyButton.addEventListener('click', function() {
-                    let originalText = textDisplay.innerText;
-                    myInput.value = originalText;
-                    myInput.style.display = 'block';
-                    myInput.select();
-                    document.execCommand('copy');
-                    myInput.style.display = 'none';
-        
-                    // Schimbă textul, adaugă stilul de text bold și culoarea de fundal
-                    textDisplay.innerText = 'COPIAT!';
-                    textDisplay.className = "copied-text";
-        
-                    // Schimbă iconița butonului și aplică noua clasă
-                    copyButton.className = "fa-solid fa-check check-button";
-                    
-                    // Revenire la textul original după 1 secundă și eliminarea stilului
-                    setTimeout(function() {
-                        textDisplay.innerText = originalText;
-                        textDisplay.className = "";
-                    }, 1000); // 1000 de milisecunde = 1 secundă
-        
-                    // Revenire la iconița originală după 1 secundă
-                    setTimeout(function() {
-                        copyButton.className = "fa-regular fa-copy copy-button";
-                    }, 1000); // 1000 de milisecunde = 1 secundă
-                });
-            }
+for (let i = 1; i <= 1000; i++) {
+    let copyButton = document.getElementById('copy-button-' + i);
+    let myInput = document.getElementById('myInput-' + i);
+    let textDisplay = document.getElementById('text-display-' + i);
+
+    copyButton.addEventListener('click', async function() {
+        let originalText = textDisplay.innerText;
+        myInput.value = originalText;
+        myInput.style.display = 'block';
+        myInput.select();
+
+        try {
+            await navigator.clipboard.writeText(originalText);
+
+            // Schimbă textul, adaugă stilul de text bold și culoarea de fundal
+            textDisplay.innerText = 'COPIAT!';
+            textDisplay.className = "copied-text";
+
+            // Schimbă iconița butonului și aplică noua clasă
+            copyButton.className = "fa-solid fa-check check-button";
+
+            // Revenire la textul original după 1 secundă și eliminarea stilului
+            setTimeout(function() {
+                textDisplay.innerText = originalText;
+                textDisplay.className = "";
+            }, 1000); // 1000 de milisecunde = 1 secundă
+
+            // Revenire la iconița originală după 1 secundă
+            setTimeout(function() {
+                copyButton.className = "fa-regular fa-copy copy-button";
+            }, 1000); // 1000 de milisecunde = 1 secundă
+        } catch (err) {
+            console.error('Eroare la copierea textului: ', err);
+        } finally {
+            myInput.style.display = 'none';
+        }
+    });
+}
+
         
         </script>
         </body>
